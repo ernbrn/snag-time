a7f916b9596e143de75efc509e85dd86d1d32392
Erin Brown <emberbrown@gmail.com>
2020-04-03 17:51:21 -0400

    fixes the thing

--- content.js
+++ content.js
@@ -1,31 +1,43 @@
 // Injected into every page visited
 
-window.onload = function () {
-  chrome.runtime.sendMessage({loaded: true}, function () {
-    console.log("let background know I'm loaded")
+window.onload = function() {
+  chrome.runtime.sendMessage({ loaded: true }, function() {
+    console.log("let background know I'm loaded");
   });
-}
+};
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "check_for_times") {
-      const alertContainer = document.getElementsByClassName("a-alert-heading");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "check_for_times") {
+    const alertContainer = document.getElementsByClassName("a-alert-heading");
 
-      if (!!alertContainer) {
-        const no = alertContainer[0];
-        if (!!no) {
-          location.reload()
+    if (!!alertContainer) {
+      const no = alertContainer[0];
+      if (!!no) {
+        if (
+          no.textContent ==
+          "No delivery windows available. New windows are released throughout the day."
+        ) {
+          location.reload();
+        } else {
+          chrome.runtime.sendMessage({ slotsA: true }, function() {
+            alert("HEY THERE ARE TIMES AVAILABLE");
+            console.log("told background there are times available");
+          });
         }
       } else {
-        chrome.runtime.sendMessage({slotsA: true}, function () {
-          alert("HEY THERE ARE TIMES AVAILABLE")
-          console.log('told background there are times available')
-        })
+        chrome.runtime.sendMessage({ slotsA: true }, function() {
+          alert("HEY THERE ARE TIMES AVAILABLE");
+          console.log("told background there are times available");
+        });
       }
+    } else {
+      chrome.runtime.sendMessage({ slotsA: true }, function() {
+        alert("HEY THERE ARE TIMES AVAILABLE");
+        console.log("told background there are times available");
+      });
     }
   }
-);
-
+});
 
 /*
 
